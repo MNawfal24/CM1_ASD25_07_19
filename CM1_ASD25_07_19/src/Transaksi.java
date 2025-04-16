c5e41750d9fdc804aa70fef5451db829cb6d1745
SenNachiii <m.nawfal.mawla@gmail.com>
2025-04-16 09:43:01 +0700

    Commit penambahan Menu

---
+++ CM1_ASD25_07_19/src/Transaksi.java
@@ -0,0 +1,39 @@
+import P6.Tugas.Dosen;
+
+public class Transaksi {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println(" ==========POLINEMA BANK==========");
+            System.out.println("\n BANK MENU:");
+            System.out.println("1. Data Norek");
+            System.out.println("2. Search Data");
+            System.out.println("3. Pencarian Transaksi (No Hp)");
+            System.out.println("4. Pencarian Maksimal Final Saldo");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih menu: ");
+            int pilih = sc.nextInt();
+            sc.nextLine();
+
+             switch (pilih) {
+             case 1:
+                
+                 break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+            
+                break;
+            case 5:
+                System.out.println("Program Selesai");
+                sc.close();
+                return;
+                default:
+                    System.out.println("Pilihan tidak valid !=");
+            }
+        }
+    }
+}
